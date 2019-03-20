96eaef25117c72445b9ce36b61cd4c1a5b4e7bd2
mkkzl <mkozal@nd.edu>
2019-03-19 20:11:21 -0400

    fixed code

--- color_make.js
+++ color_make.js
@@ -1,66 +1,24 @@
-var p= 0
-while (p<=50)
+var canvas = document.getElementById('pixel_canvas');
+var ctx = canvas.getContext("2d");
+var swidth= .4 * screen.width;
+var sheight= .4* screen.height;
+canvas.width=swidth;
+canvas.height=sheight;
+var p= 0;
+while (p<=canvas.width)
 {
 
 var i=0
-var row = document.createElement("tr")
-while (i<=100)
+while (i<=canvas.height)
 {
-  var canvas = document.createElement("canvas");
-  canvas.width =[10]
-  canvas.height= [10]
-  var column = document.createElement("td")
   var n=0
-  canvas.id = "canvas"+n;
-  var ctx = canvas.getContext("2d");
+
   var color= '#'+(Math.random()*0xFFFFFF<<0).toString(16);
   ctx.fillStyle = color;
-  ctx.fillRect(0, 0, 10, 10);
-  column.appendChild(canvas);
-  row.appendChild(column)
+  ctx.fillRect(p, i, 1, 1);
+
   i=i+1
   n=n+1
 }
-var table =document.getElementById('table')
-table.appendChild(row)
 p= p+1
 }
-
-
-/*
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var color= '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-ctx.fillStyle = color;
-ctx.fillRect(0, 0, 100, 100);
-
-var canvas = document.getElementById("myCanvas1");
-var ctx = canvas.getContext("2d");
-var color= '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-ctx.fillStyle = color;
-ctx.fillRect(0, 0, 100, 100);
-
-var canvas = document.getElementById("myCanvas2");
-var ctx = canvas.getContext("2d");
-var color= '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-ctx.fillStyle = color;
-ctx.fillRect(0, 0, 100, 100);
-
-var canvas = document.getElementById("myCanvas3");
-var ctx = canvas.getContext("2d");
-var color= '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-ctx.fillStyle = color;
-ctx.fillRect(0, 0, 100, 100);
-
-var canvas = document.getElementById("myCanvas4");
-var ctx = canvas.getContext("2d");
-var color= '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-ctx.fillStyle = color;
-ctx.fillRect(0, 0, 100, 100);
-
-var canvas = document.getElementById("myCanvas5");
-var ctx = canvas.getContext("2d");
-var color= '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-ctx.fillStyle = color;
-ctx.fillRect(0, 0, 100, 100);
-*/
--- get_color.js
+++ get_color.js
@@ -6,15 +6,11 @@ export function getData() {
   img.crossOrigin = "anonymous";
   img.onload = function() {
     var newlist = [];
-    var canvas = document.createElement('canvas');
+    var canvas = document.getElementById('image1');
     var context = canvas.getContext('2d');
-    canvas.id= 'main_image';
-    var canvas2 = document.createElement('canvas');
-    var context2 = canvas.getContext('2d');
     canvas.width = img.width;
     canvas.height = img.height;
-    canvas2.width = img.width;
-    canvas2.height = img.height;
+
     context.drawImage(img, 0, 0, img.width, img.height);
 
     var x = 0;
@@ -39,62 +35,51 @@ export function getData() {
         if (b.length == 1)
           b = "0" + b;
         newlist.push( "#" + r + g + b);
-        var j, t, i;
-        for (i = newlist.length - 1; i > 0; i--) {
-            j = Math.floor(Math.random() * (i + 1));
-            t = newlist[i];
-            newlist[i] = newlist[j];
-            newlist[j] = t;
-        }
-        //imageData = data;
-        // list.push('['+data+']');
         y= y+1;
       }
       x=x+1;
       y=0;
     }
+    var j, x, i;
+    for (i = newlist.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = newlist[i];
+        newlist[i] = newlist[j];
+        newlist[j] = x;
+    }
     console.log("Done parsing");
 
-    console.log("Nnew list: ", newlist.slice(500, 1000));
     console.log("Shuffling and redrawing pixels..");
     var w = 0;
-    while (w<=100)
+    var canvassmall = document.getElementById('newimage');
+    var ctx = canvassmall.getContext("2d");
+    canvassmall.width =img.width;
+    canvassmall.height= img.height;
+    while (w<=img.width)
     {
       var h=0
-      var row = document.createElement("tr")
-      while (h<=100)
+
+      while (h<= img.height)
       {
-        var canvassmall = document.createElement("canvas");
-        canvassmall.width =[1]
-        canvassmall.height= [1]
-        var column = document.createElement("td")
-        column.width= [1]
-        column.height= [1]
+
         var n=0
-        canvassmall.id = "canvas"+n;
-        var ctx = canvassmall.getContext("2d");
         var color= newlist[w*100 + h];
-        console.log("color: ", color, w*100 + h);
         ctx.fillStyle = color;
-        ctx.fillRect(0, 0, 1, 1);
-        column.appendChild(canvassmall);
-        row.appendChild(column)
-        // newlist.shift();
+        ctx.fillRect(w, h, 1, 1);
+
         h=h+1
         n=n+1
       }
-      var table =document.getElementById('table')
-      table.appendChild(row)
       w= w+1
     }
     console.log("Done shuffling");
+  
+
   }
 
-  img.src = "image.png";
+  img.src = "image_lg.png";
 
 }
-// var list= [];
-//var newlist=[];
-//
+
 
 getData();
